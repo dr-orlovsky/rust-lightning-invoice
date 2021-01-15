b0e3f0dcc1bb4a2404f21294f532354b8bd12e6f
Travis CI User <travis@example.org>
2021-01-15 14:02:50 +0000

    last kcov result

---
+++ target/kcov/merged/kcov-merged/index.js
@@ -0,0 +1,5 @@
+var data = {files:[
+]};
+var percent_low = 25;var percent_high = 75;
+var header = { "command" : "", "date" : "2021-01-15 14:02:50", "instrumented" : 0, "covered" : 0,};
+var merged_data = [];
